ccef5a25544d1ab7115b3294aaccc593d8fde345
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-07-24 16:20:41 +0530

    conflicts fixed and merged: sessions-events

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+Posts = new Meteor.Collection('posts');
 
+var id = 0;
 if(Meteor.isServer) {
   Meteor.methods({
     getData: function() {
@@ -10,15 +12,45 @@ if(Meteor.isServer) {
 if(Meteor.isClient) {
   Template.main.events({
     "click button": function() {
-      Meteor.call('getData', afterGetData);
+      zone.addEvent('inside-button: ' + ++id);
+      // Meteor.call('getData', afterGetData);
+      Session.set('goo', true);
     }
   });
 
-
   function afterGetData(err, data) {
     setTimeout(function() {
+      Z = zone
       var avg = data.total / data.count;
       alert(avg);
     }, 300);
   }
+
+  Template.goo.rendered = function() {
+    G = zone;
+    throw new Error("Awesome Goo");
+  };
+
+  Template.main.canShowGoo = function() {
+    return Session.get('goo') === true;
+  }
+
+  var lc = 0;
+  Posts.find().observeChanges({
+    added: function(d) {
+      if(++lc >= 2) {
+        K = zone;
+        throw new Error('high');
+      }
+    }
+  });
+
+
+  Deps.autorun(function() {
+    var goo = Session.get('goo');
+    if(goo === true) {
+      DD = zone;
+      throw new Error('Goooo Deps');
+    }
+  });
 }
